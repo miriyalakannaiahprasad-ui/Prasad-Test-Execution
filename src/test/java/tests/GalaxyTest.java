a7f2fd454291731135853709e266cbaa9c6f163b
miriyalakannaiahprasad-ui <miriyalakannaiahprasad@gmail.com>
2026-01-30 20:24:52 +0530

    Update GalaxyTest.java

--- src/test/java/tests/GalaxyTest.java
+++ src/test/java/tests/GalaxyTest.java
@@ -2,20 +2,39 @@ package tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.net.URL;
 
 public class GalaxyTest {
 
     WebDriver driver;
 
     @BeforeClass
-    public void setUp() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+    public void setUp() throws Exception {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("browserName", "Chrome");
+        caps.setCapability("browserVersion", "latest");
+        caps.setCapability("platformName", "Windows 11");
+
+        caps.setCapability("build", "Amazon Selenium Assignment");
+        caps.setCapability("name", "Galaxy Test");
+
+        caps.setCapability("selenium_version", "4.8.3");
+
+        String username = System.getenv("LT_USERNAME");
+        String accessKey = System.getenv("LT_ACCESS_KEY");
+
+        driver = new RemoteWebDriver(
+                new URL("https://" + username + ":" + accessKey + "@hub.lambdatest.com/wd/hub"),
+                caps
+        );
     }
 
     @Test
@@ -23,6 +42,9 @@ public class GalaxyTest {
         driver.get("https://www.amazon.in/");
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Samsung Galaxy");
         driver.findElement(By.id("nav-search-submit-button")).click();
+
+        String price = driver.findElement(By.cssSelector("span.a-price-whole")).getText();
+        System.out.println("Galaxy price: " + price);
     }
 
     @AfterClass
