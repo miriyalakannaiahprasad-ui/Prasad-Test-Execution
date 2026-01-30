23ef6d87968195b90f64a9ce9fe03db318574a13
miriyalakannaiahprasad-ui <miriyalakannaiahprasad@gmail.com>
2026-01-30 19:06:14 +0530

    Create GalaxyTest.java

---
+++ src/test/java/tests/GalaxyTest.java
@@ -0,0 +1,39 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class GalaxyTest {
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void searchAndAddGalaxyToCart() {
+        driver.get("https://www.amazon.com");
+
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Galaxy phone");
+        driver.findElement(By.id("twotabsearchtextbox")).submit();
+
+        driver.findElement(By.cssSelector("[data-component-type='s-search-result'] h2 a")).click();
+
+        driver.findElement(By.id("add-to-cart-button")).click();
+
+        String priceWhole = driver.findElement(By.cssSelector(".a-price-whole")).getText();
+        String priceFraction = driver.findElement(By.cssSelector(".a-price-fraction")).getText();
+
+        System.out.println("Galaxy Price: $" + priceWhole + "." + priceFraction);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+}
