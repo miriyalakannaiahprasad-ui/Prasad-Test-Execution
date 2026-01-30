47693b75a4fe77726bef904d3736d88edda8dd60
miriyalakannaiahprasad-ui <miriyalakannaiahprasad@gmail.com>
2026-01-30 20:19:21 +0530

    Update iPhoneTest.java

--- src/test/java/tests/iPhoneTest.java
+++ src/test/java/tests/iPhoneTest.java
@@ -2,39 +2,33 @@ package tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.net.URL;
-
 public class iPhoneTest {
+
     WebDriver driver;
 
     @BeforeClass
     public void setUp() {
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
     @Test
-    public void searchAndAddiPhoneToCart() {
-        driver.get("https://www.amazon.com");
-
+    public void searchIphone() {
+        driver.get("https://www.amazon.in/");
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iPhone");
-        driver.findElement(By.id("twotabsearchtextbox")).submit();
-
-        driver.findElement(By.cssSelector("[data-component-type='s-search-result'] h2 a")).click();
-
-        driver.findElement(By.id("add-to-cart-button")).click();
-
-        String priceWhole = driver.findElement(By.cssSelector(".a-price-whole")).getText();
-        String priceFraction = driver.findElement(By.cssSelector(".a-price-fraction")).getText();
-
-        System.out.println("iPhone Price: $" + priceWhole + "." + priceFraction);
+        driver.findElement(By.id("nav-search-submit-button")).click();
     }
 
     @AfterClass
     public void tearDown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
