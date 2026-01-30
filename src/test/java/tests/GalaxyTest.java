6608ad1a6c1ee841fe81952f1bcbdb235b1bd434
miriyalakannaiahprasad-ui <miriyalakannaiahprasad@gmail.com>
2026-01-30 20:19:58 +0530

    Update GalaxyTest.java

--- src/test/java/tests/GalaxyTest.java
+++ src/test/java/tests/GalaxyTest.java
@@ -2,38 +2,33 @@ package tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.net.URL;
 public class GalaxyTest {
+
     WebDriver driver;
 
     @BeforeClass
     public void setUp() {
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
     @Test
-    public void searchAndAddGalaxyToCart() {
-        driver.get("https://www.amazon.com");
-
-        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Galaxy phone");
-        driver.findElement(By.id("twotabsearchtextbox")).submit();
-
-        driver.findElement(By.cssSelector("[data-component-type='s-search-result'] h2 a")).click();
-
-        driver.findElement(By.id("add-to-cart-button")).click();
-
-        String priceWhole = driver.findElement(By.cssSelector(".a-price-whole")).getText();
-        String priceFraction = driver.findElement(By.cssSelector(".a-price-fraction")).getText();
-
-        System.out.println("Galaxy Price: $" + priceWhole + "." + priceFraction);
+    public void searchGalaxy() {
+        driver.get("https://www.amazon.in/");
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Samsung Galaxy");
+        driver.findElement(By.id("nav-search-submit-button")).click();
     }
 
     @AfterClass
     public void tearDown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
