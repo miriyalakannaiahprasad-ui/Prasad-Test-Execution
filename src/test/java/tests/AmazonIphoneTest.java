80397b154cae0bd7b1779aca65c62479372043b4
miriyalakannaiahprasad-ui <miriyalakannaiahprasad@gmail.com>
2026-01-31 17:21:45 +0530

    Update AmazonIphoneTest.java

--- src/test/java/tests/AmazonIphoneTest.java
+++ src/test/java/tests/AmazonIphoneTest.java
@@ -1,19 +1,13 @@
 package tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.*;
+import org.openqa.selenium.remote.*;
 import org.testng.annotations.*;
-
 import java.net.URL;
 import java.time.Duration;
 import java.util.List;
 
 public class AmazonIphoneTest {
-
     WebDriver driver;
 
     @BeforeMethod
@@ -24,32 +18,25 @@ public class AmazonIphoneTest {
         caps.setCapability("platformName", "Windows 11");
 
         driver = new RemoteWebDriver(
-                new URL("https://" + System.getenv("LT_USERNAME") + ":" +
-                        System.getenv("LT_ACCESS_KEY") +
-                        "@hub.lambdatest.com/wd/hub"),
-                caps
+            new URL("https://" + System.getenv("LT_USERNAME") + ":" +
+                    System.getenv("LT_ACCESS_KEY") +
+                    "@hub.lambdatest.com/wd/hub"),
+            caps
         );
-
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
     }
 
     @Test
-    public void searchIphoneAndPrintPrice() {
+    public void searchIphoneAndPrintPrice() throws InterruptedException {
         driver.get("https://www.amazon.com");
         WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
-        searchBox.sendKeys("iPhone");
-        searchBox.sendKeys(Keys.ENTER);
-
-        try { Thread.sleep(3000); } catch (InterruptedException e) { Thread.currentThread().interrupt(); }
-
+        searchBox.sendKeys("iPhone" + Keys.ENTER);
+        Thread.sleep(3000);
         List<WebElement> products = driver.findElements(By.cssSelector("h2 a"));
         if (!products.isEmpty()) products.get(0).click();
-        else { System.out.println("No products found for iPhone."); return; }
-
         List<WebElement> prices = driver.findElements(By.cssSelector("span.a-price-whole"));
         if (!prices.isEmpty()) System.out.println("iPhone Price: " + prices.get(0).getText());
-        else System.out.println("Price not found for the iPhone.");
     }
 
     @AfterMethod
