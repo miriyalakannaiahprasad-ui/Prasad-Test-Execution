f4d226d8082cfba790c247c8c4616faa29a07158
miriyalakannaiahprasad-ui <miriyalakannaiahprasad@gmail.com>
2026-01-31 09:36:20 +0530

    Update and rename iPhoneTest.java to AmazonIphoneTest.java

---
+++ src/test/java/tests/AmazonIphoneTest.java
@@ -0,0 +1,43 @@
+package tests;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.*;
+
+import java.net.URL;
+
+public class AmazonIphoneTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() throws Exception {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setBrowserName("Chrome");
+        caps.setCapability("browserVersion", "latest");
+        caps.setCapability("platformName", "Windows 11");
+
+        driver = new RemoteWebDriver(
+                new URL("https://" + System.getenv("LT_USERNAME") + ":" +
+                        System.getenv("LT_ACCESS_KEY") +
+                        "@hub.lambdatest.com/wd/hub"),
+                caps
+        );
+    }
+
+    @Test
+    public void searchIphoneAndPrintPrice() {
+        driver.get("https://www.amazon.com");
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iPhone", Keys.ENTER);
+        driver.findElements(By.cssSelector("h2 a")).get(0).click();
+
+        String price = driver.findElement(By.cssSelector("span.a-price-whole")).getText();
+        System.out.println("iPhone Price: " + price);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
